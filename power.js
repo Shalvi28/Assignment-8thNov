066d77a11d2bb7de8cb01331ae865fd25ef77a9e
Shalvi28 <92092306+Shalvi28@users.noreply.github.com>
2021-11-13 14:32:40 +0530

    Create power.js

---
+++ power.js
@@ -0,0 +1,8 @@
+//Create a function that takes in input as voltage and current and returns the power.
+function power(V,I){
+    let power = V * I;
+    return(power);
+    }
+    console.log(power(7,8)); 
+
+    
